18f8e72ffaa498d8e991b3cbc0c198c850929b4f
m.bojkic <m.bojkic@emakina.rs>
2023-02-16 12:42:25 +0100

    BONUS: Responsive header fix

--- school-project/resources/js/app.js
+++ school-project/resources/js/app.js
@@ -2,8 +2,14 @@
  * Toggle Header on mobile and tablex device size based on hamburger icon click
  * By default (on load) mobile header is closed 
 */
+
+const mainHeader = document.querySelector('.js-main-header');
+
+if (window.innerWidth < 768) {
+    mainHeader.classList.add('closed');
+}
+
 function toggleHeader() {
-    const mainHeader = document.querySelector('.js-main-header');
     if (mainHeader.classList.contains('closed')) {
         mainHeader.classList.remove('closed');
         mainHeader.style.display = 'flex';
